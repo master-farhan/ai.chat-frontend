783c4f06699b17b8423bee0f99fdee2732e589c9
Farhan Sadik <farhansadik0760@gmail.com>
2025-09-01 17:13:05 +0600

    ---

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
